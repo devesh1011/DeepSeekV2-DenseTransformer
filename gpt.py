3b249ca4e0245fc86f8fd1f5485a58210c81a172
devesh1011 <116812354+devesh1011@users.noreply.github.com>
2025-03-17 08:44:53 +0530

    Implement forward method in GPT class with optional loss calculation

--- gpt.py
+++ gpt.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 from block import Block
+import torch.nn.functional as F
 
 
 class GPT(nn.Module):
@@ -15,3 +16,22 @@ class GPT(nn.Module):
             )
         )
         self.ln_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+
+    def forward(self, idx, targets=None):
+        B, T = idx.size()
+        assert T <= self.config.block_size
+
+        tok_emb = self.transformer.wte(idx)
+
+        x = tok_emb
+
+        for block in self.transformer.h:
+            x = block(x)
+
+        x = self.transformer.ln_f(x)
+        logits = self.ln_head(x)
+        loss = None
+
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1), targets.view(-1)))
+        return logits, loss
