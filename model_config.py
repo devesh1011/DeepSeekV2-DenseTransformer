f1bf32d57b017de192a2e337ad30f4bae6ee448b
devesh1011 <116812354+devesh1011@users.noreply.github.com>
2025-03-18 17:49:06 +0530

    Update model configuration parameters for vocab size, layers, and heads

--- model_config.py
+++ model_config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     block_size: int = 1024
-    vocab_size: int = 50257
-    n_layers: int = 6
-    n_head: int = 6
+    vocab_size: int = 50304
+    n_layers: int = 4
+    n_head: int = 4
     n_embed: int = 384
