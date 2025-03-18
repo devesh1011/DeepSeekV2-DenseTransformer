ec0f8457ad207d3988cd07daf1801ec16fe40691
devesh1011 <116812354+devesh1011@users.noreply.github.com>
2025-03-18 17:50:01 +0530

    Implement weight sharing in transformer and refine weight initialization

--- gpt.py
+++ gpt.py
@@ -17,12 +17,28 @@ class GPT(nn.Module):
         )
         self.ln_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
 
+        # weight sharing scheme
+        self.transformer.wte.weight = self.ln_head.weight
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        """
+        Initialize the weights of the model with a normal distribution.
+        """
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            nn.init.normal_(module.weight, mean=0.0, std=0.006)
+        elif isinstance(module, nn.LayerNorm):
+            nn.init.constant_(module.weight, 1.0)
+            nn.init.constant_(module.bias, 0.0)
+        elif isinstance(module, nn.RMSNorm):
+            nn.init.constant_(module.weight, 1.0)
+
     def forward(self, idx, targets=None):
         B, T = idx.size()
         assert T <= self.config.block_size
 
         tok_emb = self.transformer.wte(idx)
-
         x = tok_emb
 
         for block in self.transformer.h:
@@ -30,8 +46,8 @@ class GPT(nn.Module):
 
         x = self.transformer.ln_f(x)
         logits = self.ln_head(x)
-        loss = None
 
         if targets is not None:
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1), targets.view(-1)))
-        return logits, loss
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+            return logits, loss
+        return logits
